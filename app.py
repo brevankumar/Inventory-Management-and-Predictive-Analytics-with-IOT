e4e5835b5f8d52e8c6d20ef87765e75e941575c6
brevankumar <brevankumar@gmail.com>
2024-03-05 11:50:18 +0530

    commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import os 
 import numpy as np
 import pandas as pd
-from src.mlProject.pipeline.prediction import PredictionPipeline
+from src.mlProject.pipeline.prediction import PredictionPipeline,CustomData
 
 
 app = Flask(__name__) # initializing a flask app
@@ -18,40 +18,35 @@ def training():
     return "Training Successful!" 
 
 
-@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
-def index():
-    if request.method == 'POST':
-        try:
-            #  reading the inputs given by the user
-            fixed_acidity =float(request.form['fixed_acidity'])
-            volatile_acidity =float(request.form['volatile_acidity'])
-            citric_acid =float(request.form['citric_acid'])
-            residual_sugar =float(request.form['residual_sugar'])
-            chlorides =float(request.form['chlorides'])
-            free_sulfur_dioxide =float(request.form['free_sulfur_dioxide'])
-            total_sulfur_dioxide =float(request.form['total_sulfur_dioxide'])
-            density =float(request.form['density'])
-            pH =float(request.form['pH'])
-            sulphates =float(request.form['sulphates'])
-            alcohol =float(request.form['alcohol'])
-       
-         
-            data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
-            data = np.array(data).reshape(1, 11)
-            
-            obj = PredictionPipeline()
-            predict = obj.predict(data)
+@app.route('/predict',methods=['GET','POST'])
 
-            return render_template('results.html', prediction = str(predict))
-
-        except Exception as e:
-            print('The Exception message is: ',e)
-            return 'something is wrong'
-
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('form.html')
+    
     else:
-        return render_template('index.html')
+        data=CustomData(
+            
+            #estimated_stock_pct = float(request.form.get('estimated_stock_pct')),
+            category  = request.form.get('category'),
+            temperature = float(request.form.get('temperature')),
+            quantity = float(request.form.get('quantity')),
+            unit_price = float(request.form.get('unit_price')),
+            timestamp_day_of_month = int(request.form.get('timestamp_day_of_month')),
+            timestamp_day_of_week = int(request.form.get('timestamp_day_of_week')),
+            timestamp_hour = int(request.form.get('timestamp_hour')),
+        )
+
+        final_new_data=data.get_data_as_dataframe()
+        predict_pipeline=PredictionPipeline()
+        pred=predict_pipeline.predict(final_new_data)
+
+        result = pred[0]
+        
+        return render_template('results.html',final_result=result)
 
 
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port = 8080)
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True,port=5000)
--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -7,9 +7,46 @@ from pathlib import Path
 class PredictionPipeline:
     def __init__(self):
         self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
+        self.preprocessor = joblib.load(Path('artifacts/data_transformation/preprocessor_model.joblib'))
 
     
     def predict(self, data):
-        prediction = self.model.predict(data)
+        
+        preprocessor = self.preprocessor
+        
+        data_scaled = preprocessor.transform(data)
+        
+        prediction = self.model.predict(data_scaled)
 
         return prediction
+    
+        
+
+class CustomData:
+    def __init__( self, quantity: float, temperature: float, category: str, unit_price: float, timestamp_day_of_month: int,      
+                 timestamp_day_of_week: int, timestamp_hour: int):
+        
+       # self.estimated_stock_pct=estimated_stock_pct
+        self.quantity= quantity
+        self.temperature = temperature      
+        self.category = category   
+        self.unit_price = unit_price 
+        self.timestamp_day_of_month  = timestamp_day_of_month    
+        self.timestamp_day_of_week  = timestamp_day_of_week  
+        self.timestamp_hour =  timestamp_hour         
+        
+
+    def get_data_as_dataframe(self):
+            custom_data_input_dict = {
+               # 'estimated_stock_pct':[self.estimated_stock_pct],
+                'quantity':[self.quantity],
+                'temperature':[self.temperature],
+                'category':[self.category],
+                'unit_price':[self.unit_price],
+                'timestamp_day_of_month':[self.timestamp_day_of_month],
+                'timestamp_day_of_week':[self.timestamp_day_of_week],
+                'timestamp_hour':[self.timestamp_hour]
+            }
+            
+            return pd.DataFrame(custom_data_input_dict)
+        
